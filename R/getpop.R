2d91cbd690407c004524cf6c3086caa3ff65ddbd
Lucie Contamin <contamin.lucie@gmail.com>
2019-07-19 15:03:24 +0700

    Update to remove mcdev dependency

--- R/getpop.R
+++ R/getpop.R
@@ -1,3 +1,15 @@
+# Download file
+# insipired from function download of the package mcdev on github
+# "choisy/mcdev"
+download <- function(url, package, file, ...) {
+  path <- find.package(package)
+  destdir <- paste0(path, "/extdata")
+  if (!dir.exists(destdir)) dir.create(destdir)
+  utils::download.file(url, paste0(destdir, "/", file), ...)
+  invisible(url)
+}
+
+# ------------------------------------------------------------------------------
 #' Get local human population density
 #'
 #' Retrieves a raster file containing the local human population density of
@@ -10,7 +22,6 @@
 #'
 #' @export
 #' @importFrom raster writeRaster values values<-
-#' @importFrom mcdev download
 #' @importFrom utils installed.packages
 #'
 getpop <- function(year = 2009, adjusted = FALSE) {
@@ -19,7 +30,8 @@ getpop <- function(year = 2009, adjusted = FALSE) {
                     "VNM_ppp_v2b_2010", "VNM_ppp_v2b_2015_UNadj",
                     "VNM_ppp_v2b_2015", "VNM_ppp_v2b_2020_UNadj")) {
     x <- get(object)
-    filename <- paste0(installed.packages()["worldpopVN", "LibPath"], "/worldpopVN/extdata/", object, ".tif")
+    filename <- paste0(installed.packages()["worldpopVN", "LibPath"],
+                       "/worldpopVN/extdata/", object, ".tif")
     x@file@name <- filename
     if (!file.exists(filename)) {
       message("WorldPop data are not on disk.")
@@ -27,14 +39,18 @@ getpop <- function(year = 2009, adjusted = FALSE) {
       ans <- readline()
       if (ans %in% c("y", "")) {
         fname <- basename(filename)
-        message(paste0("Do you want to download the original file (603.8 MB) or a compressed version (",
-                       worldpopVN:::sizes[fname], " MB)? o / c (default)"))
-        message("Note: a compressed version will require additional compututing (< 60'') for uncompressing.")
+        message(paste0(
+          "Do you want to download the original file (603.8 MB) or a compressed version (",
+          worldpopVN:::sizes[fname], " MB)? o / c (default)"))
+        message(
+          "Note: a compressed version will require additional compututing (< 60'') for uncompressing.")
         ans <- readline()
         if (ans == "o")
-          download(grep(fname, worldpopVN:::urlso, value = TRUE), "worldpopVN", fname, mode = "wb")
+          download(grep(fname, worldpopVN:::urlso, value = TRUE), "worldpopVN",
+                   fname, mode = "wb")
         else {
-          download(grep(fname, worldpopVN:::urlsc, value = TRUE), "worldpopVN", fname, mode = "wb")
+          download(grep(fname, worldpopVN:::urlsc, value = TRUE), "worldpopVN",
+                   fname, mode = "wb")
           message("Rescaling the data...")
           values(x) <- values(x) / 1e4
           message("Writing rescaled data to disk...")
