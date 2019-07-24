5514fec4ba4662e4771109914f04df64368a9957
Lucie Contamin <contamin.lucie@gmail.com>
2019-07-24 19:36:20 +0700

    style + add example

--- R/getpop.R
+++ R/getpop.R
@@ -15,15 +15,21 @@ download <- function(url, package, file, ...) {
 #' Retrieves a raster file containing the local human population density of
 #' Vietnam.
 #'
-#' @param year A numeric reflecting the year to retrieve the data. Could be 2009,
-#' 2010, 2015 or 2020. 2009 is incompatible with adjusted = TRUE.
+#' @param year A numeric reflecting the year to retrieve the data. Could be
+#' 2009, 2010, 2015 or 2020. 2009 is incompatible with adjusted = TRUE.
 #' @param adjusted A logical indicating whether the data should be adjusted to
-#' match UN population divisions estimates. TRUE is incompatible with year = 2009.
+#' match UN population divisions estimates. TRUE is incompatible with
+#' year = 2009.
 #'
 #' @export
 #' @importFrom raster writeRaster values values<-
 #' @importFrom utils installed.packages
 #'
+#' @examples
+#' popvn_2009 <- getpop()
+#'
+#' # Or for 2020 with ajusted value to match UN population divisions estimates :
+#'popvn_a2020 <- getpop(2020, adjusted = TRUE)
 getpop <- function(year = 2009, adjusted = FALSE) {
   object <- paste0("VNM_ppp_v2b_", year, ifelse(adjusted, "_UNadj", ""))
   if (object %in% c("VNM_ppp_v2b_2009", "VNM_ppp_v2b_2010_UNadj",
@@ -40,17 +46,21 @@ getpop <- function(year = 2009, adjusted = FALSE) {
       if (ans %in% c("y", "")) {
         fname <- basename(filename)
         message(paste0(
-          "Do you want to download the original file (603.8 MB) or a compressed version (",
-          worldpopVN:::sizes[fname], " MB)? o / c (default)"))
+          "Do you want to download the original file (603.8 MB) or a ",
+          "compressed version (", mget("sizes", inherits = TRUE)[[1]][fname],
+          " MB)? o / c (default)"))
         message(
-          "Note: a compressed version will require additional compututing (< 60'') for uncompressing.")
+          "Note: a compressed version will require additional compututing ",
+          "(< 60'') for uncompressing.")
         ans <- readline()
-        if (ans == "o")
-          download(grep(fname, worldpopVN:::urlso, value = TRUE), "worldpopVN",
-                   fname, mode = "wb")
-        else {
-          download(grep(fname, worldpopVN:::urlsc, value = TRUE), "worldpopVN",
-                   fname, mode = "wb")
+        if (ans == "o") #nocov start
+          download(
+            grep(fname, mget("urlso", inherits = TRUE)[[1]], value = TRUE),
+            "worldpopVN", fname, mode = "wb")
+        else { #nocov end
+          download(
+            grep(fname,mget("urlsc", inherits = TRUE)[[1]], value = TRUE),
+            "worldpopVN", fname, mode = "wb")
           message("Rescaling the data...")
           values(x) <- values(x) / 1e4
           message("Writing rescaled data to disk...")
